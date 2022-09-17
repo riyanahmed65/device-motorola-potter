29655b714ace3a43bce954845409aca3a29e4fc8
riyanahmed65 <mohd.riyan65@gmail.com>
2022-09-17 18:59:26 +0530

    potter: disable secure element

--- device.mk
+++ device.mk
@@ -390,8 +390,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.1 \
     android.hardware.radio.config@1.0.vendor \
     android.hardware.radio.config@1.0 \
-    android.hardware.secure_element@1.0.vendor \
-    android.hardware.secure_element@1.0 \
     librmnetctl \
     libprotobuf-cpp-full \
     libxml2 \
